ac0d536d893be56c0290e88b54decb6489719819
Ryan Lewis <1994ryanlewis1994@gmail.com>
2019-04-10 21:09:25 +0100

    changes

--- server.js
+++ server.js
@@ -10,27 +10,37 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'pass',
+		database: 'smart-brain'
+	}
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res) => {
+	res.send('Working');
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+	register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+	profile.handleProfileGet(req, res, db);
+});
+app.put('/image', (req, res) => {
+	image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+	image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+app.listen(process.env.PORT || 3000, () => {
+	console.log('app is running');
+});
